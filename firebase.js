c3a723f3ffae18bb1c5182ff9c4eb2e5ad1fc649
Ivaylo <ivaylopehlivanov@abv.bg>
2021-05-28 17:56:34 +0300

    adding checkout page

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBmL-bDP5MhfRTd8escXBInXN5n_5B7pdw",
+    authDomain: "clone-eb67d.firebaseapp.com",
+    projectId: "clone-eb67d",
+    storageBucket: "clone-eb67d.appspot.com",
+    messagingSenderId: "791110328522",
+    appId: "1:791110328522:web:da7d9ea4f0039440931433",
+    measurementId: "G-BKJ324Z4HN"
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import Image from 'next/image';
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 const Header = () => {
+    const [session] = useSession();
+    const router = useRouter();
+    const items = useSelector(selectItems);
+
     return (
         <header className="">
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="flex items-center flex-grow sm:flex-grow-0 mt-2">
                     <Image
+                        onClick={() => router.push('/')}
                         className="cursor-pointer"
                         src="https://links.papareact.com/f90"
                         width={150}
@@ -19,16 +28,18 @@ const Header = () => {
                     <SearchIcon className="h-12 p-4" />
                 </div>
                 <div className="flex items-center text-xs space-x-6 mx-6 whitespace-nowrap text-white">
-                    <div className="link">
-                        <p>Hello Ivaylo Pehlivanov</p>
+                    <div className="link" onClick={!session ? signIn : signOut}>
+                        <p>
+                            {session ? `Hello ${session.user.name}` : 'Sign In'}
+                        </p>
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
                     <div className="link">
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    <div className="relative flex items-center link">
-                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                    <div className="relative flex items-center link" onClick={() => router.push('/checkout')}>
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">{items.length}</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
                     </div>
@@ -36,7 +47,7 @@ const Header = () => {
             </div>
             <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
                 <p className="link flex items-center">
-                    <MenuIcon className="h-6 mr-1"/>
+                    <MenuIcon className="h-6 mr-1" />
                     All
                 </p>
                 <p className="link">Prime Video</p>
