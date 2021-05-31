6d6218480e081bcaf362ef37bdecf91a693e809b
Ivaylo <ivaylopehlivanov@abv.bg>
2021-06-01 01:25:05 +0300

    fining stripe

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         domains: ['links.papareact.com', 'fakestoreapi.com']
     },
     env: {
-        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+        stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
         STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
         STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET
     }
