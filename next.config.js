f6e8baad9dd74d308e45d4a89c6db5de2c299da0
Ivaylo <ivaylopehlivanov@abv.bg>
2021-06-01 01:30:36 +0300

    fixing Stripe

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     env: {
         stripe_public_key: `${process.env.STRIPE_PUBLIC_KEY}`,
-        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-        STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET
+        STRIPE_SECRET_KEY: `${process.env.STRIPE_SECRET_KEY}`,
+        STRIPE_SIGNING_SECRET: `${process.env.STRIPE_SIGNING_SECRET}`
     }
 }
