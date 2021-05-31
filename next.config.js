de252303b6392df31dd3cfb0f85527cb66e2b513
Ivaylo <ivaylopehlivanov@abv.bg>
2021-06-01 01:19:19 +0300

    fixing env.variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
         domains: ['links.papareact.com', 'fakestoreapi.com']
     },
     env: {
-        stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET
     }
 }
